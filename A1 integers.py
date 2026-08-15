f2b6aae04d07c7b72d42d7b87c4ef47ec088569a
manikantagokavalasa-cmd <manikantagokavalasa@gmail.com>
2026-08-15 21:06:42 +0530

    Add files via upload

---
+++ A1 integers.py
@@ -0,0 +1,12 @@
+<class 'int'>
+Age in 2050: 45
+age = 21
+current_year = 2026
+birth_year = current_year - age
+
+print(type(age))
+print(type(current_year))
+print(type(birth_year))
+print("Age in 2050:", 2050 - birth_year)
+x = 17
+y = 5
