fc8623257a023c0623feec15808a7cd289e93ce7
manikantagokavalasa-cmd <manikantagokavalasa@gmail.com>
2026-09-21 11:55:25 +0530

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,9 @@
+countries = ("India", "Japan", "Canada", "Brazil", "Germany", "Australia")
+
+print(countries)
+print(type(countries))
+print(len(countries))
+#out put:
+('India', 'Japan', 'Canada', 'Brazil', 'Germany', 'Australia')
+<class 'tuple'>
+6
